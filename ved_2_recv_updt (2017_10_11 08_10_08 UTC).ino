050de6057299a7e4e5cd3cd0328f4d9353081aed
Vivek Patel <40636968+OverthinkTank@users.noreply.github.com>
2019-05-02 12:27:13 -0700

    Add files via upload
    Ardunio File for receiver

---
+++ ved_2_recv_updt (2017_10_11 08_10_08 UTC).ino
@@ -0,0 +1,39 @@
+#include <SoftwareSerial.h>
+
+int green = 7;
+int blue = 12; // red is connected to 13
+char inP;
+
+void setup()
+{
+  Serial.begin(9600);//Setting the baud rate of Serial Monitor
+  pinMode(blue, OUTPUT);
+  pinMode(green, OUTPUT);
+  digitalWrite(13, HIGH);
+}
+
+void loop()
+{
+
+  if (Serial.available())
+  {
+
+    inP = Serial.read();//string
+    if (inP == '1')
+    {
+
+      Serial.write(inP);
+      Serial.println("accepted");
+      delay(2000);
+      digitalWrite(13, LOW);
+      digitalWrite(blue, HIGH);
+      delay(4000);
+      digitalWrite(blue, LOW);
+      digitalWrite(green, HIGH);
+      delay(10000);
+      digitalWrite(green, LOW);
+      digitalWrite(13, HIGH);
+    }
+    Serial.write("2");
+  }
+}
