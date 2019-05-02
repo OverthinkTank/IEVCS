b2d2df9401f662331eb0c7763bf5a6119780b262
Vivek Patel <40636968+OverthinkTank@users.noreply.github.com>
2019-05-02 12:28:00 -0700

    Add files via upload
    Arduino file for Sender

---
+++ ved_2 (2017_10_06 17_31_14 UTC).ino
@@ -0,0 +1,32 @@
+ #include <SoftwareSerial.h>
+String a;//for input from RFID
+int i = 1;
+//SoftwareSerial mySerial(9,10);//RX,TX
+SoftwareSerial mySerialRF(11,12);//RX,TX
+void setup()
+{
+  
+  Serial.begin(9600); //Setting the baud rate of Serial Monitor 
+  /*while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }*/
+  //mySerial.begin(9600); // Setting the baud rate of Software Serial Library  
+  mySerialRF.begin(9600); // Setting the baud rate of Software Serial Library  
+ }
+void loop()
+{
+    
+ if(mySerialRF.available())
+ {
+    a = mySerialRF.readString();
+    if(a == "5300B8BC4611")
+    {
+      Serial.println(i);
+    //  mySerial.write(i);//send to remote over hc12
+      digitalWrite(13,HIGH);
+      delay(3000);
+      digitalWrite(13,LOW);
+    }
+ // Serial.write(mySerial.read());
+  }
+}
